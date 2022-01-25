e3ea5e983f8daa909587f763403ea69fd28dd97f
Fei Ye <feiye@vims.edu>
2022-01-24 23:44:13 -0500

    More work.

---
+++ schism_py_pre_post/Grid/Grid_geometry.py
@@ -0,0 +1,18 @@
+from pylib import inside_polygon
+import shapefile
+import numpy as np
+
+
+def find_points_in_polyshp(pt_xy, shapefile_names):
+    ind = np.zeros(pt_xy[:, 0].shape)
+    for shapefile_name in shapefile_names:
+        # shapefile # records = sf.records() # sf.shapeType # len(sf) # s = sf.shape(idx)
+        sf = shapefile.Reader(shapefile_name)
+        shapes = sf.shapes()
+
+        for shp in shapes:
+            poly_xy = np.array(shp.points).T
+            ind += inside_polygon(pt_xy, poly_xy[0], poly_xy[1])  # 1: true; 0: false
+
+    ind = ind.astype('bool')
+    return ind
