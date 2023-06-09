8060034d9a93298cfffa2aa0aaaa340250a9520f
Fei Ye <feiye@vims.edu>
2023-06-09 00:44:33 -0400

    Added README, including installation instructions.

--- schism_py_pre_post/spp-experimental/setup.py
+++ schism_py_pre_post/spp-experimental/setup.py
@@ -4,12 +4,13 @@ with open('requirements.txt', 'r') as f:
   requirements = f.read().splitlines()
 
   setup(
-    name='spp-essentials',
+    name='spp-experimental',
     version='0.0.1',
     description='An sub package of the experimental package of "Python tools for pre/post-processing SCHISM models"',
     license='MIT',
     packages=[
-      'spp-essentials',
+      'spp-experimental',
+      'spp-experimental.spp-essentials',
     ],
     package_data={},
     install_requires=requirements,
